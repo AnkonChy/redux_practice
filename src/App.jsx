ed418d119db6ca64879f3d95bec9968e663cf50b
Ankon Chowdhury <ankonchy1@gmail.com>
2025-03-28 15:03:24 +0600

    redux added succesfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,49 @@
-import { useState } from "react";
 import Counter from "./components/Counter";
 import Stats from "./components/Stats";
+import { useDispatch, useSelector } from "react-redux";
+import { increment } from "./features/counters/countersSlice";
 
-const initialCounters = [
-  {
-    id: 1,
-    value: 0,
-  },
-  {
-    id: 2,
-    value: 0,
-  },
-];
+// const initialCounters = [
+//   {
+//     id: 1,
+//     value: 0,
+//   },
+//   {
+//     id: 2,
+//     value: 0,
+//   },
+// ];
 
 function App() {
-  const [counters, setCounters] = useState(initialCounters);
+  const counters = useSelector((state) => state.counters);
+  // const [counters, setCounters] = useState(initialCounters);
 
   const totalCount = counters.reduce((sum, current) => sum + current.value, 0);
-
+  const dispatch = useDispatch();
   const handleIncrement = (counterId) => {
-    const updatedCounters = counters.map((counter) => {
-      if (counter.id === counterId) {
-        return {
-          ...counter,
-          value: counter.value + 1,
-        };
-      }
-      return counter;
-    });
-    setCounters(updatedCounters);
+    dispatch(increment(counterId));
+    // const updatedCounters = counters.map((counter) => {
+    //   if (counter.id === counterId) {
+    //     return {
+    //       ...counter,
+    //       value: counter.value + 1,
+    //     };
+    //   }
+    //   return counter;
+    // });
+    // // setCounters(updatedCounters);
   };
   const handleDecrement = (counterId) => {
-    const updatedCounters = counters.map((counter) => {
-      if (counter.id === counterId) {
-        return {
-          ...counter,
-          value: counter.value - 1,
-        };
-      }
-      return counter;
-    });
-    setCounters(updatedCounters);
+    // const updatedCounters = counters.map((counter) => {
+    //   if (counter.id === counterId) {
+    //     return {
+    //       ...counter,
+    //       value: counter.value - 1,
+    //     };
+    //   }
+    //   return counter;
+    // });
+    // setCounters(updatedCounters);
   };
   return (
     <div className="w-screen h-screen mx-auto border border-red-400 p-10 bg-gray-100 text-slate-700">
